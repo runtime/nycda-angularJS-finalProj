54eb2c8653f80a99c12c8ac75808205684e85f03
Erik <erik.kroha@craftww.com>
2014-08-13 19:25:02 -0400

    first commit generators

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name nycdaAngularJsFinalProjApp
+ * @description
+ * # nycdaAngularJsFinalProjApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('nycdaAngularJsFinalProjApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nycdaAngularJsFinalProjApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the nycdaAngularJsFinalProjApp
+ */
+angular.module('nycdaAngularJsFinalProjApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
